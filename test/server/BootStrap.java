f4a2ea44a4f2bad5ed8ee3fc42ed9a8463641c59
liusu_sis@os.ucweb.com <liusu_sis@os.ucweb.com>
2016-02-23 17:51:21 +0800

    first

---
+++ test/server/BootStrap.java
@@ -0,0 +1,29 @@
+package server;
+
+import java.io.File;
+
+import play.Logger;
+import play.Play;
+import play.server.Server;
+
+public class BootStrap {
+	
+	public static void main(String[] args) throws Exception {
+//        File root = new File(System.getProperty("application.path"));
+        String appPath = "F:\\workspace\\play-self"; 
+        File root = new File(appPath);
+        if (System.getProperty("precompiled", "false").equals("true")) {
+            Play.usePrecompiled = true;
+        }
+        if (System.getProperty("writepid", "false").equals("true")) {
+            Server.writePID(root);
+        }
+        Play.init(root, System.getProperty("play.id", ""));
+        if (System.getProperty("precompile") == null) {
+            new Server(args);
+        } else {
+            Logger.info("Done.");
+        }
+    }
+
+}
---
+++ test/server/Main.java
@@ -0,0 +1,50 @@
+package server;
+
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import play.Invoker;
+import play.Logger;
+import play.Play;
+import play.classloading.ApplicationClasses;
+import play.classloading.ApplicationClassloader;
+import play.classloading.ApplicationClasses.ApplicationClass;
+import play.plugins.PluginCollection;
+import play.server.Server;
+import play.server.PlayHandler.NettyInvocation;
+import play.vfs.VirtualFile;
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		String appPath = "F:\\workspace\\play-self"; 
+		File root = new File(appPath); 
+        Play.init(root, System.getProperty("play.id", ""));
+        
+        Play.classloader = new ApplicationClassloader();
+        Play.initialized = true;
+//		  1:        
+//        Play.detectChanges();
+        
+//        2:
+//        Play.classloader.detectChanges(); // 
+//        Play.classloader.getAllClasses();
+        
+//        3:
+        List<ApplicationClass> all = new ArrayList<ApplicationClass>();
+    	for (VirtualFile virtualFile : Play.javaPath) {
+    		Play.classloader.getAllClasses(virtualFile);
+        }
+    	
+    	for (ApplicationClass applicationClass : Play.classes.all()) {
+            Class clazz = Play.classloader.loadApplicationClass(applicationClass.name);
+        }
+        
+    }
+}
